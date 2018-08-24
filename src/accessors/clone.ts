3d00ccc7096a802e8ae05e430ece448e066dfa38
Cahil Foley <cahilfoley2@gmail.com>
2018-08-24 13:53:15 +0800

    Fixed silly typescript stuff

--- src/accessors/clone.ts
+++ src/accessors/clone.ts
@@ -1,5 +1,5 @@
 interface ICloneOptions {
-  copyFuncs: bool
+    copyFuncs?: boolean
 }
 
 /**
@@ -7,38 +7,38 @@ interface ICloneOptions {
  * @param original The value to clone
  * @param options Config options
  */
-const clone = (original: any, { copyFuncs }: ICloneOptions) => {
-  // Can't clone functions, only copy if the flag is set
-  if (typeof original === 'function') {
-    return copyFuncs ? original : {}
-  }
-  
-  // Nulls will be caught as objects later so return them now
-  if (original === null) {
-    return null 
-  }
-
-  // If the input is a date, create a new one with the same value
-  if (original instanceof Date) {
-    return new Date(original.valueOf())
-  }
-
-  // If the input is an array, clone each item
-  if (Array.isArray(original)) {
-    return original.map(clone)
-  }
-
-  // If the input is an object, clone each value onto a new object
-  if (typeof original === 'object') {
-    const output = {}
-    for (const [key, value] of Object.entries(original)) {
-      output[key] = clone(value)
+const clone = (original: any, { copyFuncs }: ICloneOptions = {}): any => {
+    // Can't clone functions, only copy if the flag is set
+    if (typeof original === 'function') {
+        return copyFuncs ? original : {}
     }
 
-    return output
-  }
+    // Nulls will be caught as objects later so return them now
+    if (original === null) {
+        return null
+    }
 
-  return original
+    // If the input is a date, create a new one with the same value
+    if (original instanceof Date) {
+        return new Date(original.valueOf())
+    }
+
+    // If the input is an array, clone each item
+    if (Array.isArray(original)) {
+        return original.map(clone)
+    }
+
+    // If the input is an object, clone each value onto a new object
+    if (typeof original === 'object') {
+        const output = {}
+        for (const [key, value] of Object.entries(original)) {
+            output[key] = clone(value)
+        }
+
+        return output
+    }
+
+    return original
 }
 
 export default clone
