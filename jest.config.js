ff3364031d28d4a2b663a0e27c1936a750d09506
Cahil Foley <cahilfoley2@gmail.com>
2018-09-19 00:37:06 +0800

    Update build and deploy configuration and prepare for version 1.0.5

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,9 @@
-const {
-  name
-} = require('./package')
+const { name } = require('./package')
 
 module.exports = {
   collectCoverage: true,
-  collectCoverageFrom: [
-    'src/**/*.{ts,tsx}',
-    '!**/node_modules/**',
-    '!**/index.ts'
-  ],
-  coverageDirectory: 'dist/coverage',
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!**/node_modules/**', '!**/index.ts'],
+  coverageDirectory: './.temp/coverage',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   name,
   testEnvironment: 'node',
