e924c7641c782fd061a0cce694217247c3e13dcb
Cahil Foley <cahilfoley2@gmail.com>
2018-09-21 01:04:00 +0800

    :white_check_mark: Add new function `flatMap`

--- src/array/index.ts
+++ src/array/index.ts
@@ -1,3 +1,4 @@
 export { default as filterBy } from './filterBy'
+export { default as flatMap } from './flatMap'
 export { default as partitionArray } from './partitionArray'
 export { default as updateItem } from './updateItem'
