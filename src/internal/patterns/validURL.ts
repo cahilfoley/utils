0c2a363d05b6303ee4de520b4a2e22213b7fbc9d
Cahil Foley <cahilfoley2@gmail.com>
2018-10-19 12:29:30 +0800

    fix(internal): Fix URL pattern to match local

--- src/internal/patterns/validURL.ts
+++ src/internal/patterns/validURL.ts
@@ -1,14 +1,4 @@
-/** Regular expression to match URL patterns, developed using this handy tool https://regexr.com/3sju0 */
-const urlRegex: RegExp = new RegExp(
-  [
-    '^(https?:\\/\\/)', // Protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|((d{1,3}.){3}d{1,3}))', // Host name or IP
-    '(\\:\\d+)?', // Optional port
-    '(\\/[-a-z\\d%_.~+]*)*', // Path
-    '(\\?[;&a-z\\d%_.~+=-]*)?', // Optional query string
-    '(\\#[-a-z\\d_]*)?$' // Optional anchor hash
-  ].join(''),
-  'i'
-)
+/** Regular expression to match URL patterns, developed using this handy tool https://regex101.com/r/lQ1nI3/1 */
+const urlRegex: RegExp = /^(http[s]?:\/\/)?([^:\/\s]+)(:([^\/]*))?(\/\w+\.)*([^#?\s]+)(\?([^#]*))?(#(.*))?$/i
 
 export default urlRegex
