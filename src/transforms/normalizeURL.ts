f10d5468628c345f724d6ffabab802524d74c47a
Cahil Foley <cahilfoley2@gmail.com>
2019-08-22 22:58:48 +0800

    refactor(transforms): split out methods to reduce complexity

--- src/transforms/normalizeURL.ts
+++ src/transforms/normalizeURL.ts
@@ -6,10 +6,43 @@
  * Normalizes the number of slashes in the protocol section of a url
  * @private
  */
-function normalizeProtocolSlashes(url: string) {
+function normalizeProtocol(urlParts: string[]) {
+  // If the first part is a plain protocol we combine it with the next part
+  if (urlParts[0].match(/^[^/:]+:\/*$/) && urlParts.length > 1) {
+    // Strip any colon or slashes from the protocol
+    const protocol = urlParts.shift().replace(/:\/*/, '')
+    // Join with two slashes, next section will convert to 3 for file protocol if needed
+    urlParts[0] = `${protocol}://${urlParts[0]}`
+  }
+
   // There must be two or three slashes in the file protocol, two slashes in anything else.
-  const isFile = url.match(/^file:\/+/)
-  return url.replace(/^([^/:]+):\/*/, isFile ? '$1:///' : '$1://')
+  const isFile = urlParts[0].match(/^file:\/+/)
+  urlParts[0] = urlParts[0].replace(/^([^/:]+):\/*/, isFile ? '$1:///' : '$1://')
+
+  return urlParts
+}
+
+/**
+ * Normalizes the number of slashes in each section of the URL
+ * @private
+ */
+function normalizeSectionSlashes(urlParts: string[]) {
+  return urlParts.flatMap((section, i) => {
+    if (typeof section !== 'string') {
+      throw new TypeError(
+        `URL must be a string. Received ${section} as argument index ${i} which is type ${typeof section}`,
+      )
+    }
+
+    // Skip empty sections
+    if (section === '') return []
+
+    // Removing the starting slashes for each component but the first.
+    if (i > 0) section = section.replace(/^[/]+/, '')
+
+    // Removing the ending slashes for each component but the last.
+    return section.replace(/[/]+$/, i < urlParts.length - 1 ? '' : '/')
+  })
 }
 
 /**
@@ -28,43 +61,14 @@ function normalizeProtocolSlashes(url: string) {
  *
  */
 export default function noramlizeURL(...urlParts: string[]): string {
-  const resultArray = []
-
   // Ignore empty strings at the start
-  while (urlParts[0].trim() === '') {
-    urlParts.shift()
-  }
+  while (urlParts[0].trim() === '') urlParts.shift()
 
-  // If the first part is a plain protocol we combine it with the next part
-  if (urlParts[0].match(/^[^/:]+:\/*$/) && urlParts.length > 1) {
-    // Strip any colon or slashes from the protocol
-    const protocol = urlParts.shift().replace(/:\/*/, '')
-    // Join with two slashes, next section will convert to 3 for file protocol if needed
-    urlParts[0] = `${protocol}://${urlParts[0]}`
-  }
+  // Make sure the start protocol section has the correct number of slashes
+  urlParts = normalizeProtocol(urlParts)
 
-  urlParts[0] = normalizeProtocolSlashes(urlParts[0])
-
-  for (let i = 0; i < urlParts.length; i++) {
-    let section = urlParts[i]
-
-    if (typeof section !== 'string') {
-      throw new TypeError(
-        `URL must be a string. Received ${section} as argument index ${i} which is type ${typeof section}`,
-      )
-    }
-
-    // Skip empty sections
-    if (section === '') continue
-
-    // Removing the starting slashes for each component but the first.
-    if (i > 0) section = section.replace(/^[/]+/, '')
-
-    // Removing the ending slashes for each component but the last.
-    section = section.replace(/[/]+$/, i < urlParts.length - 1 ? '' : '/')
-
-    resultArray.push(section)
-  }
+  // Normalize the number of slashes between each section of the url
+  const resultArray = normalizeSectionSlashes(urlParts)
 
   // Each input component is now separated by a single slash except the possible first plain protocol part.
   // Remove trailing slash before parameters or hash
